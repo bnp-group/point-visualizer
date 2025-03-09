05ca2b23f06d31eacc8a22a90df3da1fbc54ed19
Bartosz Bieniek <itsbk20@gmail.com>
2025-03-09 17:28:52 +0100

    Run visualizer in main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import visualizer
+from visualizer import Visualizer
+
+
+def main():
+    with open("input.txt") as f:
+        visualization = visualizer.load(f)
+
+    Visualizer.visualize(visualization)
+
+
+if __name__ == "__main__":
+    main()
